24ba05f3cd2e5ed9fa3671820a22ceb6a4689df7
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2023-06-11 15:35:48 +0600

    jwt token verify setup and use

--- index.js
+++ index.js
@@ -9,8 +9,38 @@ const stripe = require("stripe")(
 
 const port = process.env.PORT || 5000;
 
+// idleware ------------------------------
+
 app.use(cors());
 app.use(express.json());
+
+// jwt midleware ------------------------------
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+
+  // bearer token
+
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.User_Access_Token, function (err, decoded) {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+
+    // set decoded in req
+
+    req.decoded = decoded;
+    next();
+  });
+};
 
 console.log(process.env.ACCESS_TOKEN);
 
